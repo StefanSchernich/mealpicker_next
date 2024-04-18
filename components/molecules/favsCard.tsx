764ef64d6972da71880be3ba5aa4a517adb19ef0
Stefan Schernich <stefan.schernich@gmail.com>
2024-04-18 20:48:29 +0200

    Add modal to Favs

--- components/molecules/favsCard.tsx
+++ components/molecules/favsCard.tsx
@@ -8,10 +8,21 @@ import {
 import { Heart } from "lucide-react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
+import DishCard from "../organisms/DishCard";
 
 export default function FavsCard({ dish }: { dish: Dish }) {
   // #region State & Effects
   const [isLiked, setIsLiked] = useState(false);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   useEffect(() => {
     // On initial render: Check if the dish is already liked
@@ -41,38 +52,63 @@ export default function FavsCard({ dish }: { dish: Dish }) {
     };
   }, [dish]);
   // #region Return
-  // TODO: Add modal for viewing details of dish
   return (
-    <div className="flex w-full gap-4 py-2">
-      <div className="relative min-h-24 min-w-24">
-        {dish.imgUrl ? (
-          <Image
-            src={dish.imgUrl}
-            alt={dish.title}
-            fill
-            className="object-cover"
-          />
-        ) : (
-          <div className="flex h-full w-full items-center justify-center rounded-md bg-gray-700">
-            No Image
-          </div>
-        )}
-      </div>
-      <div className="flex grow flex-col items-start gap-2">
-        <h2 className="text-md">{dish.title}</h2>
-        <div className="*:text-md flex w-auto gap-4 rounded-full bg-gray-900 p-2">
-          <p>{getIcon(dish.category, categoryOptions)}</p>
-          <p>{getIcon(dish.calories, caloryOptions)}</p>
-          <p>{getIcon(dish.difficulty, difficultyOptions)}</p>
+    <>
+      <div className="flex w-full gap-4 py-2">
+        <div className="relative min-h-24 min-w-24">
+          {dish.imgUrl ? (
+            <Image
+              src={dish.imgUrl}
+              alt={dish.title}
+              fill
+              className="object-cover"
+              onClick={onOpen}
+            />
+          ) : (
+            <div
+              className="flex h-full w-full items-center justify-center rounded-md bg-gray-700"
+              onClick={onOpen}
+            >
+              No Image
+            </div>
+          )}
         </div>
+        <div className="flex grow flex-col items-start gap-2" onClick={onOpen}>
+          <h2 className="text-md">{dish.title}</h2>
+          <div className="*:text-md flex w-auto gap-4 rounded-full bg-gray-900 p-2">
+            <p>{getIcon(dish.category, categoryOptions)}</p>
+            <p>{getIcon(dish.calories, caloryOptions)}</p>
+            <p>{getIcon(dish.difficulty, difficultyOptions)}</p>
+          </div>
+        </div>
+        <Heart
+          className="min-h-6 min-w-6 self-center"
+          color="red"
+          fill={`${isLiked ? "red" : "none"}`}
+          onClick={() => toggleLike(dish)}
+          cursor={"pointer"}
+        />
       </div>
-      <Heart
-        className="min-h-6 min-w-6 self-center"
-        color="red"
-        fill={`${isLiked ? "red" : "none"}`}
-        onClick={() => toggleLike(dish)}
-        cursor={"pointer"}
-      />
-    </div>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        scrollBehavior="inside"
+      >
+        <ModalContent className="bg-gray-950">
+          {(onClose) => (
+            <>
+              <ModalBody>
+                <DishCard
+                  retrievedDish={dish}
+                  isImageLoaded={true}
+                  setRetrievedDish={() => {}}
+                  setIsImageLoaded={() => {}}
+                />
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
